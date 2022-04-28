e0186646ca2976960f775f79c909c7cdf824f5eb
Dilshod Uskanov <dilshoduskanov@gmail.com>
2022-04-28 10:01:32 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:instagram_clone_app/pages/home_page.dart';
+import 'package:instagram_clone_app/pages/sign_in_page.dart';
+import 'package:instagram_clone_app/pages/sign_up_page.dart';
+import 'package:instagram_clone_app/pages/splash_page.dart';
+import 'package:instagram_clone_app/services/hive_service.dart';
+
+Future<void> main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(HiveDB.DB_NAME);
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  // notification
+  const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    description:
+    'This channel is used for important notifications.', // description
+    importance: Importance.max,
+  );
+  var initAndroidSetting =
+  const AndroidInitializationSettings("@mipmap/ic_launcher");
+  var initIosSetting = const IOSInitializationSettings();
+  var initSetting =
+  InitializationSettings(android: initAndroidSetting, iOS: initIosSetting);
+  await FlutterLocalNotificationsPlugin().initialize(initSetting);
+  await FlutterLocalNotificationsPlugin()
+      .resolvePlatformSpecificImplementation<
+      AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp])
+      .then((value) => runApp(const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  Widget _startPage() {
+    return StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (BuildContext context, snapshot) {
+          if (snapshot.hasData) {
+            HiveDB.storeUid(snapshot.data!.uid);
+            return const SplashPage();
+          } else {
+            HiveDB.removeUid();
+            return const SignInPage();
+          }
+        });
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Instagram Clone',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: _startPage(),
+      routes: {
+        SplashPage.id: (context) => SplashPage(),
+        SignInPage.id: (context) => SignInPage(),
+        SignUpPage.id: (context) => SignUpPage(),
+        HomePage.id: (context) => HomePage()
+      },
+    );
+  }
+}
